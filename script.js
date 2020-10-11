c917befdbb82d2bbe5165bfc0ae48ebc85c04d17
Josue Narro <josue.narro.a@gmail.com>
2020-10-11 16:47:26 -0500

    Refactor code

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const keys = document.querySelectorAll(".key");
-
 function playSound(event) {
   const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
   const sound = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  if (sound) {
-    sound.play();
-    key.classList.add("playing");
-  }
+
+  if (!sound) return;
+
+  sound.currentTime = 0;
+  sound.play();
+  key.classList.add("playing");
+  setTimeout(() => {
+    key.classList.remove("playing");
+  }, 70);
 }
 
 window.addEventListener("keydown", playSound);
